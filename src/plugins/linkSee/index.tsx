cef0b6fcf20e4ed4dfa09dc910e7de1b9b515834
EncryptingCoder <111210474+EncryptingCoder@users.noreply.github.com>
2025-09-13 00:00:03 -0500

    Basic plugin definition

---
+++ src/plugins/linkSee/index.tsx
@@ -0,0 +1,14 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+  name: "LinkSee",
+  description: "Adds a distinction between masked links and regular links.",
+  authors: [Devs.fearless],
+});
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -606,6 +606,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "thororen",
         id: 848339671629299742n
     },
+    fearless: {
+        name: "FearlessDudeiscool",
+        id: 750913974614228996n,
+    }
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
